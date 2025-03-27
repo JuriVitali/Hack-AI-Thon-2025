9924f39a08d66424ce5075a87ac8a7b074b1e799
fppcng <fppcng@gmail.com>
2025-03-27 11:16:04 +0100

    Aggiunta app flask base

---
+++ backend/app.py
@@ -0,0 +1,35 @@
+from flask import request, jsonify, render_template, flash, request, redirect, url_for, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from config import app, db, allowed_file
+from werkzeug.utils import secure_filename
+import os
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename) # never trust user input
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file', name=filename))
+    return render_template('index.html')
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    
+    app.run(host='0.0.0.0', debug=True)
+
---
+++ backend/config.py
@@ -0,0 +1,25 @@
+from flask import Flask, flash, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(app) # disabilita errore e rende possibili le richieste tra backend e frontend
+
+# UPLOAD_FOLDER = 'backend/uploads' 
+UPLOAD_FOLDER = os.path.join(app.root_path, 'uploads')
+
+ALLOWED_EXTENSIONS = {'pdf'}
+
+def allowed_file(filename):  # ritorna vero se c'è il punto nel nome e se l'estensione è tra quelle consentite, falso altrimenti
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
